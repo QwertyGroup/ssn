3873039138dee574308945a0a27b71b36fabfb20
Arseniy Kucher <beeffallo@gmail.com>
2019-08-30 20:31:11 +0300

    assemble|disassemble

--- ssn.py
+++ ssn.py
@@ -3,23 +3,38 @@ from skimage import io
 import matplotlib.pyplot as plt
 
 
-def main():
+def disassemble():
+    print('disassembling')
     source = io.imread('source.png')
     print(f'type: {type(source)}')  # np.ndarray
     print(f'shage: {source.shape}')
     print(f'size: {source.size / (1 << 20):.2f} MB')
     image = source[:, :, 0].copy()
+    # image = source[:, :, :].copy()
     for i in range(8)[::-1]:
         layer = 1 << i
-        mask = image > layer
+        mask = image >= layer
         image[mask] -= layer
-        plt.imsave(f'layer{layer}.png', image, cmap='gray')
+        plt.imsave(f'layer{layer}.png', mask, cmap='gray')
         print(f'layer{layer} saved')
 
-    # image[mask] = 255
-    # image[~mask] = 0
-    # image[100:200, 100:200, :] = 0
-    # plt.imshow(image, cmap='gray')
+
+def assemble():
+    print('assembling')
+    source = None
+    for i in range(8)[::-1]:
+        layer = 1 << i
+        if source is None:
+            source = io.imread(f'layer{layer}.png').astype(bool) * layer
+        else:
+            source += io.imread(f'layer{layer}.png').astype(bool) * layer
+        print(f'layer{layer} embedded')
+    plt.imshow(source, cmap='gray')
+
+
+def main():
+    disassemble()
+    assemble()
 
 
 if __name__ == '__main__':
