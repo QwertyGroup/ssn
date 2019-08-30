57ec72be4fe6ef3b7d0b59d05bbb94902bc2dfe4
Arseniy Kucher <beeffallo@gmail.com>
2019-08-30 21:06:23 +0300

    rgb

--- ssn.py
+++ ssn.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import uint8
 from skimage import io
 import matplotlib.pyplot as plt
 
@@ -6,16 +6,15 @@ import matplotlib.pyplot as plt
 def disassemble():
     print('disassembling')
     source = io.imread('source.png')
-    print(f'type: {type(source)}')  # np.ndarray
+    print(f'type: {type(source)}')
     print(f'shage: {source.shape}')
     print(f'size: {source.size / (1 << 20):.2f} MB')
-    image = source[:, :, 0].copy()
-    # image = source[:, :, :].copy()
+    image = source[:, :, :].copy()
     for i in range(8)[::-1]:
         layer = 1 << i
         mask = image >= layer
         image[mask] -= layer
-        plt.imsave(f'layer{layer}.png', mask, cmap='gray')
+        plt.imsave(f'layer{layer}.png', mask.astype(dtype=uint8)*uint8(255))
         print(f'layer{layer} saved')
 
 
@@ -24,16 +23,20 @@ def assemble():
     source = None
     for i in range(8)[::-1]:
         layer = 1 << i
+        def load(): return io.imread(
+            f'layer{layer}.png').astype(bool) * uint8(layer)
         if source is None:
-            source = io.imread(f'layer{layer}.png').astype(bool) * layer
+            source = load()
         else:
-            source += io.imread(f'layer{layer}.png').astype(bool) * layer
+            source += load()
+        plt.imsave(f'asm{(1 << 8) - (1 << i)}.png', source)
         print(f'layer{layer} embedded')
-    plt.imshow(source, cmap='gray')
+    plt.imshow(source)  # , cmap='gray')
+    plt.imsave('composed.png', source)
 
 
 def main():
-    disassemble()
+    # disassemble()
     assemble()
 
 
