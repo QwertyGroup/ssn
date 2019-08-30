9964aa38ed8e5a93704c91ebc7bcc23974a75ef5
Arseniy Kucher <beeffallo@gmail.com>
2019-08-30 14:58:33 +0300

    loading image

--- ssn.py
+++ ssn.py
@@ -1,13 +1,19 @@
-from skimage import data, filters
+import numpy as np
+from skimage import io
 import matplotlib.pyplot as plt
 
 
 def main():
-    print('main')
-    image = data.coins()   # ... or any other NumPy array!
-    edges = filters.sobel(image)
-    plt.imshow(edges, cmap='gray')
+    image = io.imread('source.png')
+    print(f'type: {type(image)}')  # np.ndarray
+    print(f'shage: {image.shape}')
+    print(f'size: {image.size / (1 << 20):.2f} MB')
+    mask = image < 87
+    image[mask] = 255
+    plt.imshow(image, cmap='gray')
 
 
 if __name__ == '__main__':
+    print('main')
     main()
+    plt.show()
